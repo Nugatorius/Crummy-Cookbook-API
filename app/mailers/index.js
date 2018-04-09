23b609a0a7f446d1b584728d8cede59d4b73f6d7
Aritra Ghosh <aritra119.26@gmail.com>
2018-04-09 12:06:27 +0530

    Feature/add joi validation (#5)
    * Added Joi
    
    * Added mailer boilerplate

---
+++ app/mailers/index.js
@@ -0,0 +1,33 @@
+const mailer = require('nodemailer');
+const passwordGenerator = require('generate-password');
+require('./utils/logger');
+process.env.NODE_ENV === 'test' ? require('dotenv').config({ path: '.test.env' }) : require('dotenv').config();
+
+const sendPassword = async (to, from = process.env.USER_EMAIL, subject) => {
+  try {
+    const transporter = mailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.USER_EMAIL,
+        pass: process.env.USER_PASSWORD
+      }
+    });
+    const password = passwordGenerator.generate({
+      length: 16,
+      numbers: true
+    });
+    const mailOptions = {
+      from: from,
+      to: to,
+      subject: subject,
+      text: password
+    };
+    await transporter.sendMail(mailOptions);
+    return password;
+  }
+  catch (exception) {
+    throw new Error(exception);
+  }
+};
+
+export default { sendPassword };
